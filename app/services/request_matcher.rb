82100c2d3f56c61689c136f4882384b4ef01af65
Darima Pivovarova <dpivovarova@gmail.com>
2023-09-27 15:03:50 +0200

    playing around with geo distances

--- app/services/request_matcher.rb
+++ app/services/request_matcher.rb
@@ -1,23 +1,38 @@
 class RequestMatcher
+
+  require 'geokit-rails'
   # Matching logic based on 1) The partner should be experienced with the materials the customer requests
-  # 2) The customer is within the operating radius of the partner
+  # 2) The request lon and lat is within the operating radius of the partner
   def self.match_requests_to_partners(request)
     # Matching logic here
+    # Calculating using postgresql and PostGIS extension
     # partners_within_radius = Partner
-    # .where(
-    #   "ST_Distance(
-    #      ST_GeographyFromText('SRID=4326;POINT(' || partners.lon || ' ' || partners.lat || ')'),
-    #      ST_GeographyFromText('SRID=4326;POINT(:lon :lat)')
-    #    ) <= operating_radius",
-    #   lon: request.lon,
-    #   lat: request.lat
-    # )
+    #   .where(
+    #     "ST_Distance(
+    #       ST_GeographyFromText('POINT(:request_lon :request_lat)', 4326),
+    #       ST_GeographyFromText('POINT(' || partners.lon || ' ' || partners.lat || ')', 4326)
+    #     ) <= partners.operating_radius",
+    #     request_lon: request.lon,
+    #     request_lat: request.lat
+    #     # operating_radius: operating_radius
+    #     # operating_radius: request.operating_radius
+    #   )
 
-    # # Filter partners based on materials experience
-    # suitable_partners = partners_within_radius
-    # .where(is_wood: request.is_wood, is_carpet: request.is_carpet, is_tiles: request.is_tiles)
+      partners_within_radius = Partner
+      .where(
+        Geocoder::Calculations.distance_between([request.lat,request.lat],
+          [partners.lat,partners.lon], :units => :m) < partners.operating_radius
+      )
 
-    suitable_partners = Partner.where(is_wood: request.is_wood, is_carpet: request.is_carpet, is_tiles: request.is_tiles)
+      # .where("operating_radius >= :operating_radius", operating_radius: request.operating_radius)
+
+
+
+    # Filter partners based on materials experience
+    suitable_partners = partners_within_radius
+    .where(is_wood: request.is_wood, is_carpet: request.is_carpet, is_tiles: request.is_tiles)
+
+    # suitable_partners = Partner.where(is_wood: request.is_wood, is_carpet: request.is_carpet, is_tiles: request.is_tiles)
 
     suitable_partners
 
